553231e2e68cf1270e3a5fb5e87d576424a65c6c
Délivré <delivrewelo@gmail.com>
2024-12-01 22:00:48 +0300

    last change

--- BookManager/Services/BookService.cs
+++ BookManager/Services/BookService.cs
@@ -46,9 +46,9 @@ namespace BookManager.Services
                 var fakerLanguage = new Faker(settings.LanguageRegion);
                 var books = new List<Book>();
 
-                int startIndex = (settings.PageNumber - 1) * 10 + 1;
+                int startIndex = (settings.PageNumber - 1) * 5 + 1;
 
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i < 5; i++)
                 {
                     var book = PopulateBook(i, startIndex, fakerLanguage, settings);
                     books.Add(book);
@@ -74,12 +74,13 @@ namespace BookManager.Services
                 AvgLikes = settings.AvgLikes,
                 AvgReviews = settings.AvgReviews,
                 ImageUrl = faker.Image.LoremFlickrUrl(),
-                Reviews = GenerateReviews(faker, (int)settings.AvgReviews),
+                Reviews = GenerateReviews(faker, settings.AvgReviews),
             };
         }
-        private string[] GenerateReviews(Faker f, int reviewCount)
+        private string[] GenerateReviews(Faker f, double reviewCount)
         {
-            var reviews = new string[reviewCount];
+            var reviewChecker = reviewCount <= 0.5 ? 1 : (int)reviewCount;
+            var reviews = new string[reviewChecker];
             for (int i = 0; i < reviews.Length; i++)
             {
                 reviews[i] = f.Lorem.Sentence();
